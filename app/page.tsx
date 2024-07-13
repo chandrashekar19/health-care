f91090dcff83fb4b2bab54f2edd8d35ea964df38
KalalChandu <kalalshannu19@gmail.com>
2024-07-13 19:18:55 +0530

    Implemented: the admin page for careplus

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+import PatientForm from '@/components/forms/PatientForm';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl underline">Hello</h1>
+    <div className='flex h-screen max-h-screen'>
+      <section className='remove-scrollbar container my-auto'>
+        <div className='sub-container max-w-[496px]'>
+          <Image
+            src='/assets/icons/logo-full.svg'
+            height={1000}
+            width={1000}
+            alt='patient'
+            className='mb-12 h-10 w-fit'
+          />
+          <PatientForm />
+          <div className='text-14 regular mt-20 justify-between'>
+            <p className='justify-items-end text-dark-600 xl:text-left'>
+              © 2024 CarePulse
+            </p>
+            <Link href='/?admin=true' className='text-green-500'></Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src='/assets/images/onboarding-img.png'
+        height={1000}
+        width={1000}
+        alt='pateint'
+        className='side-img max-w-[50%]'
+      />
     </div>
   );
 }
